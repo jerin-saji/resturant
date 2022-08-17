64ebf755c89ea6dac9d50f42a668d17da0974cfc
jerin <5.jerinsaji@gmail.com>
2022-08-17 11:16:34 +0530

    updated--

--- restaurant/master/urls.py
+++ restaurant/master/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.homeview ,name='home'),
     path('items/', views.itemview ,name='items-list'),
     path('items/<pk>', views.itemview ,name='items-delete'),
+    path('update-items/<str:pk>', views.itemupdate ,name='items-update'),
     path('customer/', views.customerview ,name='customer-list'),
     path('customer/<pk>', views.customerview ,name='customer-delete'),
+    path('update-customer/<str:pk>', views.customerupdate ,name='customer-update'),
 ]
--- restaurant/master/views.py
+++ restaurant/master/views.py
@@ -30,6 +30,22 @@ def itemview(request, pk=None):
         
         return render(request,'master/item_form.html',{'form' :form,'lists': list2})
 
+def itemupdate(request,pk):
+    ls = Item.objects.get(id=pk)
+    form = Itemform(instance=ls)
+
+    if request.method == 'POST':
+        form = Itemform(request.POST,instance=ls)
+        if form.is_valid():
+            form.save()
+            return redirect('/items/')
+
+    context = {'form':form}
+    
+    return render(request,'master/update_item.html',context)
+
+
+
 
 def customerview(request,pk=None):   
     if request.method =='POST':
@@ -40,11 +56,27 @@ def customerview(request,pk=None):
         return redirect(customerview)
 
     elif request.method == 'GET':
+        form = Customerform
         if pk!=None:
             item_to_delete = Customer.objects.get(id=pk)
             item_to_delete.delete()
-        form = Customerform
         cust_list = list(Customer.objects.all().values())
             
         
         return render(request,'master/customer_form.html',{'form' :form,'key1': cust_list})
+
+
+def customerupdate(request,pk):
+
+    ls = Customer.objects.get(id=pk)
+    form = Customerform(instance=ls)
+
+    if request.method == 'POST':
+        form = Customerform(request.POST,instance=ls)
+        if form.is_valid():
+            form.save()
+            return redirect('/customer/')
+
+    context = {'form':form}
+    
+    return render(request,'master/update_customer.html',context)
